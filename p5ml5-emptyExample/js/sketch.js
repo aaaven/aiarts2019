5aec29cc01492426e9b263f8b6c5abe84116b4c4
Aven Le Zhou <aven@artmachines.net>
2019-09-16 12:14:16 +0800

    add empty example

---
+++ p5ml5-emptyExample/js/sketch.js
@@ -0,0 +1,13 @@
+// Aven Le Zhou https://www.aven.cc
+// IMA, NYU Shanghai
+// Artificial Intelligence Arts Fall 2019 https://wp.nyu.edu/shanghai-ima-aiarts/
+
+function setup() {
+  createCanvas(800, 600);
+  background(0);
+}
+
+
+function draw() {
+  //
+}
